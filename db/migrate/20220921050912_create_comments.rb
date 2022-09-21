5d2bf6a501c9a18dc1b90b49f8ac9324e9b4b903
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-21 08:14:46 +0300

    Fix linter errors

--- db/migrate/20220921050912_create_comments.rb
+++ db/migrate/20220921050912_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :post,  foreign_key: { to_table: :posts }
+      t.references :post, foreign_key: { to_table: :posts }
       t.references :author, foreign_key: { to_table: :users }
 
       t.timestamps

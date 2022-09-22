8607df794f7badd280cb4d8e507f2c78cfb14b2f
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-22 11:47:37 +0300

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name:'User'
-    belongs_to :post
-    
-    after_create :update_comments_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-   def update_comments_counter
-        post.update(comments_counter: post.comments.count)
-   end
+  after_create :update_comments_counter
 
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name:'User'
-    has_many :comments, foreign_key: :post_id
-    has_many :likes, foreign_key: :post_id
-  
-    after_create :update_posts_counter
-    after_destroy :update_posts_counter
-   def update_posts_counter
-        author.update(posts_counter: author.posts.count)
-   end
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
 
-   def most_recent_comments
-      comments.order(created_at: :desc).limit(5)
-   end
+  after_create :update_posts_counter
+  after_destroy :update_posts_counter
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

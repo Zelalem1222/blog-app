d66fd9c374c3505f10dc015dfbc6cc9136f5bc9b
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-22 12:22:56 +0300

    Make changes based on the revewer comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,14 @@ class Post < ApplicationRecord
 
   after_create :update_posts_counter
   after_destroy :update_posts_counter
-  def update_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end

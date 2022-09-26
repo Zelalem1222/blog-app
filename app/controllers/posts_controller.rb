85fbf2142fd61feb52a642443de89ac7d4cdbfb5
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-26 05:51:43 +0300

    Fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
-class PostsController < ApplicationController 
+class PostsController < ApplicationController
   def index
-    @posts = 'here are the posts of the user with id ' + params[:user_id]
+    @posts = "here are the posts of the user with id #{params[:user_id]}"
   end
 
   def show
-    @post = 'here is the post with id ' + params[:id]
+    @post = "here is the post with id #{params[:id]}"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
-    def index
-    end
+  def index; end
 
-    def show
-        @user = 'here is the profile of the user'
-    end
+  def show
+    @user = 'here is the profile of the user'
+  end
 
-    def post
-        @posts = 'here are the posts of the user with id ' + params[:id]
-
-    end
+  def post
+    @posts = "here are the posts of the user with id #{params[:id]}"
+  end
 end

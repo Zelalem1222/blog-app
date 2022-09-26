98af8c9d61f56fc28798cd4793c9a2d445e615d3
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-26 06:13:27 +0300

    Make change based on reviers comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@
 Rails.application.routes.draw do
   
    root to: "users#index"
-   get 'users/:id', to: "users#show"
-   get 'users/:user_id/posts', to: "posts#index"
-   get 'users/:user_id/posts/:id', to: "posts#show"
+
+   resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+    end
 end

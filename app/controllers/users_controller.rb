ede7d61b14f7d58e8e3df4949b14cc8bcc678f6a
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-26 05:49:08 +0300

    Create user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    def index
+    end
+
+    def show
+        @user = 'here is the profile of the user'
+    end
+
+    def post
+        @posts = 'here are the posts of the user with id ' + params[:id]
+
+    end
+end

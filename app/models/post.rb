70f44eddf928bb39fbe901bf5a617ea3710f9de7
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-28 15:48:30 +0300

    Make final change

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,20 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :post_id
 
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  def most_recent_likes
-    likes.order(created_at: :desc).limit(5)
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 } 
+
+  after_create :update_posts_counter
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
+
+  private
 
   def update_posts_counter
     author.increment!(:posts_counter)
   end
 end
+
+
